9d59c19eeaaad9f4bc88403eb92258d7976e2385
eswaribala <parameswaribala@gmail.com>
2022-05-16 17:00:35 +0530

    updated

--- customer/src/main/java/com/boa/customer/controllers/CustomerController.java
+++ customer/src/main/java/com/boa/customer/controllers/CustomerController.java
@@ -12,11 +12,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.boa.customer.models.Customer;
 import com.boa.customer.models.FullName;
 import com.boa.customer.services.CustomerService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.bohnman.squiggly.Squiggly;
+import com.github.bohnman.squiggly.util.SquigglyUtils;
 import com.google.gson.Gson;
 
 @RestController
@@ -103,5 +107,33 @@ public class CustomerController {
 				return ResponseEntity.status(HttpStatus.BAD_REQUEST)
 						.body("Customer Not Found");
 		}
+		
+		
+
+	    @GetMapping({"/v1.0/filters/{customerId}"})
+	    public ResponseEntity<String> getUserFilteredDataById(@PathVariable("customerId") 
+	    long customerId,@RequestParam(name = "fields", required = false) String fields) {
+	    	Customer customerObj=this.customerService.getCustomerById(customerId);
+	    	
+	    	
+	    	if(customerObj!=null)
+	    	{
+	    		//fields refers to runtime selection
+	    		ObjectMapper mapper = Squiggly.init(new ObjectMapper(), fields);     		
+				return ResponseEntity.status(HttpStatus.ACCEPTED).
+						body(SquigglyUtils.stringify(mapper, customerObj));
+				
+
+	    	}
+	    	else
+	    	{
+	    		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Customer Not Available");
+	    	}
+
+	    }
+
+		
+		
+		
 		
 }
