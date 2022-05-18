769743c4aa54256808d7f6dc37277fd88b68f1db
eswaribala <parameswaribala@gmail.com>
2022-05-18 15:43:44 +0530

    updated

---
+++ zuulserver/src/main/java/com/boa/zuulserver/ZuulserverApplication.java
@@ -0,0 +1,13 @@
+package com.boa.zuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulserverApplication.class, args);
+	}
+
+}
---
+++ zuulserver/src/test/java/com/boa/zuulserver/ZuulserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boa.zuulserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
