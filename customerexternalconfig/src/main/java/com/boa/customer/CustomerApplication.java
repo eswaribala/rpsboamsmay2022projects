29c4a510fb1325b8fcfbcdcb2aa880333988fd71
eswaribala <parameswaribala@gmail.com>
2022-05-18 15:09:40 +0530

    updated

--- customerexternalconfig/src/main/java/com/boa/customer/CustomerApplication.java
+++ customerexternalconfig/src/main/java/com/boa/customer/CustomerApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
+@EnableEurekaClient
 @SpringBootApplication
 public class CustomerApplication {
 
